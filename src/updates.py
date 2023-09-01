f371f2806b85109499883001d2d2ec707b72fa4f
Diogo Regateiro <diogoregateiro@ua.pt>
2023-09-01 13:10:39 +0100

    Reuse cache between updates.

--- src/updates.py
+++ src/updates.py
@@ -1,15 +1,16 @@
 import apt
 
+CACHE = apt.Cache()
+
 def get_apt_updates(config: dict) -> dict:
     # Update APT cache
     print('Updating APT cache...') 
-    cache = apt.Cache()
-    cache.update()
-    cache.open(None)
+    CACHE.update()
+    CACHE.open(None)
 
     # Process APT packages
     upgrades = {}
-    for package in [p for p in cache if p.is_installed and p.name in config['packages']['apt']]:
+    for package in [p for p in CACHE if p.is_installed and p.name in config['packages']['apt']]:
         current = [v.version for v in package.versions if v.is_installed][0]
         latest = package.versions[0].version
         upgrades[package.name] = {
